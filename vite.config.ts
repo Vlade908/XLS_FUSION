b4198c6d1e142282b1095f3e0eb910408077f787
Enzo Alves <80132168+Vlade908@users.noreply.github.com>
2026-02-04 15:40:57 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    proxy: {
+      // Quando o front chamar /api, o Vite redireciona para o Backend
+      '/api': {
+        target: 'http://localhost:5000', // Certifique-se que seu servidor node usa esta porta
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
